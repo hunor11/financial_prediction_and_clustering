2d4adb873901c0865686ba0e641319a2ef8af606
Hunor Acs <hunii197@gmail.com>
2024-12-30 12:30:19 +0200

    delete currencies page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,25 @@
 // src/components/Navbar.jsx
-import React from 'react';
-import { AppBar, Toolbar, Button, Box } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <AppBar 
-      position="static"
-      height={'10vh'}
-    >
+    <AppBar position="static" height={"10vh"}>
       <Toolbar>
         <Box sx={{ flexGrow: 1 }}>
-          <Button color="inherit" onClick={() => navigate('/')}>
+          <Button color="inherit" onClick={() => navigate("/")}>
             Home
           </Button>
-          <Button color="inherit" onClick={() => navigate('/stocks')}>
+          <Button color="inherit" onClick={() => navigate("/stocks")}>
             Stocks
           </Button>
-          <Button color="inherit" onClick={() => navigate('/currencies')}>
-            Currencies
-          </Button>
-          <Button color="inherit" onClick={() => navigate('/news')}>
+          <Button color="inherit" onClick={() => navigate("/news")}>
             News
           </Button>
-          <Button color="inherit" onClick={() => navigate('/clustering')}>
+          <Button color="inherit" onClick={() => navigate("/clustering")}>
             Clustering
           </Button>
         </Box>
